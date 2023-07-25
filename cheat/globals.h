18472456b8a1ef45b29289e4dbfc36ae6af381fe
ridhwan2 <110493782+ridhwan2@users.noreply.github.com>
2023-07-25 13:11:25 -0400

    Update globals.h

--- cheat/globals.h
+++ cheat/globals.h
@@ -7,7 +7,7 @@ namespace globals
 	inline std::uintptr_t clientAddress = 0;
 
 	inline bool glow = false;
-	inline float glowColor[] = { 255.f, 0.f, 0.f, 255.f };
+	inline float glowColor[] = { 1.0f, 0.0f, 0.0f, 1.0f };
 
 	inline bool radar = false;
 	inline bool bhop = false;
