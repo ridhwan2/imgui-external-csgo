32679ccba8b68498fa41e33f2dab55383226ba61
ridhwan2 <110493782+ridhwan2@users.noreply.github.com>
2023-07-25 11:19:19 -0400

    Update hacks.cpp

--- cheat/hacks.cpp
+++ cheat/hacks.cpp
@@ -6,6 +6,13 @@
 
 void hacks::VisualsThread(const Memory& mem) noexcept
 {
+	struct Vector2
+	{
+		float x = { }, y = { };
+	};
+
+	auto oldPunch = Vector2{ };
+
 	while (gui::isRunning)
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(0));
@@ -70,7 +77,52 @@ void hacks::VisualsThread(const Memory& mem) noexcept
 						mem.Write<BYTE>(client + offsets::dwForceJump, 6);
 				}
 			}
+
+			if (globals::rcs)
+			{
+				const auto memory = Memory{ "csgo.exe" };
+
+				const auto client = memory.GetModuleAddress("client.dll");
+				const auto engine = memory.GetModuleAddress("engine.dll");
+
+				const auto& localPlayer = memory.Read<std::uintptr_t>(client + offsets::dwLocalPlayer);
+				const auto& shotsFired = memory.Read<std::int32_t>(localPlayer + offsets::m_iShotsFired);
+
+				if (shotsFired)
+				{
+					const auto& clientState = memory.Read<std::uintptr_t>(engine + offsets::dwClientState);
+					const auto& viewAngles = memory.Read<Vector2>(clientState + offsets::dwClientState_ViewAngles);
+
+					const auto& aimPunch = memory.Read<Vector2>(localPlayer + offsets::m_aimPunchAngle);
+
+					auto newAngles = Vector2
+					{
+						viewAngles.x + oldPunch.x - aimPunch.x * 2.f,
+						viewAngles.y + oldPunch.y - aimPunch.y * 2.f
+					};
+
+					if (newAngles.x > 89.f)
+						newAngles.x = 89.f;
+
+					if (newAngles.x < -89.f)
+						newAngles.x = -89.f;
+
+					while (newAngles.y > 180.f)
+						newAngles.y -= 360.f;
+
+					while (newAngles.y < -180.f)
+						newAngles.y += 360.f;
+
+					memory.Write<Vector2>(clientState + offsets::dwClientState_ViewAngles, newAngles);
+
+					oldPunch.x = aimPunch.x * 2.f;
+					oldPunch.y = aimPunch.y * 2.f;
+				}
+				else
+				{
+					oldPunch.x = oldPunch.y = 0.f;
+				}
+			}
 		}
 	}
 }
-
